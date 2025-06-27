5389b00b3303ae24008b174c7b3c670a6ca16f97
williamhdz94 <williamhernandez450@gmail.com>
2025-06-27 07:10:35 -0600

    Validators dinamyc - Add elements in form array

--- src/app/reactive/pages/dinamyc-page/dinamyc-page.component.ts
+++ src/app/reactive/pages/dinamyc-page/dinamyc-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormsUtils } from '../../../utils/forms-utils';
 
 @Component({
@@ -26,6 +26,24 @@ export class DinamycPageComponent {
 
   get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  newFavorite = new FormControl('', Validators.required);
+
+  onAddToFavorites() {
+    if( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(this.fb.control( newGame, Validators.required ));
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite(i: number) {
+    this.favoriteGames.removeAt(i);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
   }
 
 }
